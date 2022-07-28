393895c521051eafe064fa8bd94e83e307ed09e2
Beatriz Friso de Paula <beatriz_f.p@hotmail.com>
2022-07-28 16:01:02 -0300

    criamos o nosso repositório

---
+++ WebAPIAspNetCore/StatusCode/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StatusCode.Models;
+
+namespace StatusCode.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private SistemaContext DbSistema = new SistemaContext();
+
+        [HttpGet]
+        public void RequererTodos()
+        {
+
+        }
+
+        [HttpGet]
+        public void RequererUmPelaId(int Id)
+        {
+
+        }
+
+        [HttpPost]
+        public void PublicarUm(Usuario Usuario)
+        {
+        
+        }
+
+        [HttpDelete]
+        public void DeletarUmPelaId(int Id, Usuario Usuario)
+        {
+
+        }
+
+        [HttpPut]
+        public void SubstituirUmPelaId(int Id, Usuario Usuario)
+        {
+
+        }
+    }
+}
